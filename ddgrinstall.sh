a5140c7470b58331a6941de81e2cc7eadcfeb482
Lutxo Italico <aleatoriosdenario@gmail.com>
2024-07-10 09:02:58 +0000

    Update ddgrinstall.sh

--- ddgrinstall.sh
+++ ddgrinstall.sh
@@ -1,3 +1,10 @@
-sudo apt update && sudo apt install snapd
+# Installando duckdukgo dalla linea di commandi
+# s'installa anche anche snapd
 
+sudo apt update && sudo apt install snapd
 snap install ddgr
+
+# Uso:
+# scrivi 
+# ddgr pizza margherita 
+# per trovare risultati relativi alla pizza margherita
