20c2bf4a5ca98868a7d11bb729f518f61e2806ca
Lutxo Italico <aleatoriosdenario@gmail.com>
2024-07-11 08:46:47 +0000

    Update torbrowserinstall.sh

--- torbrowserinstall.sh
+++ torbrowserinstall.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-echo "[+] Installando Tor"
+echo "[+] Preparando l'installazione di Tor Browser"
 sudo apt install curl xz-utils
+rm -rf tor.* tor-* sta* Bro*
+echo "[+] Scaricando binari e firme"
+wget https://www.torproject.org/dist/torbrowser/13.5.1/tor-browser-linux-x86_64-13.5.1.tar.xz
+wget https://www.torproject.org/dist/torbrowser/13.5.1/tor-browser-linux-x86_64-13.5.1.tar.xz.asc
 echo "[+] Verificando firme"
 gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
 gpg --output ./tor.keyring --export EF6E286DDA85EA2A4BA7DE684E2C6E8793298290
-gpgv --keyring ./tor.keyring ~/Downloads/tor-browser-linux-x86_64-13.0.15.tar.xz.asc ~/Downloads/tor-browser-linux-x86_64-13.0.15.tar.xz
+gpgv --keyring ./tor.keyring ~/Downloads/tor-browser/tor-browser-linux-x86_64-13.5.1.tar.xz.asc ~/Downloads/tor-browser/tor-browser-linux-x86_64-13.5.1.tar.xz
 curl -s https://openpgpkey.torproject.org/.well-known/openpgpkey/torproject.org/hu/kounek7zrdx745qydx6p59t9mqjpuhdf |gpg --import -
-echo "[+] Extracting TOR"
-tar -xf tor-browser-linux-x86_64-13.0.15.tar.xz -v
 echo "[!] Verifica se le firme sono corrette prima di iniziare ad utilizzare Tor"
+sleep 3
+echo "[+] Extracting TOR"
+tar -xf tor-browser-linux-x86_64-13.5.1.tar.xz -v
+rm tor-browser-linux-x86_64-13.5.1.tar.xz
+
