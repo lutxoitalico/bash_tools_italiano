a500ff2164e3f7580cadb7b0a51f3f63d5a41a90
Lutxo <aleatoriosdenario@gmail.com>
2024-07-17 10:26:25 +0200

    Update pcstatus.sh

--- pcstatus.sh
+++ pcstatus.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Colours
-c01="\e[0;31m\033[1m"
-c02="\e[0;33m\033[1m"
-c03="\e[0;32m\033[1m"
-c04="\e[0;36m\033[1m"
-c05="\e[0;34m\033[1m"
-c06="\e[0;35m\033[1m"
-c07="\e[0;37m\033[1m"
+co1="\e[0;31m\033[1m"
+co2="\e[0;33m\033[1m"
+co3="\e[0;32m\033[1m"
+co4="\e[0;36m\033[1m"
+co5="\e[0;34m\033[1m"
+co6="\e[0;35m\033[1m"
+co7="\e[0;37m\033[1m"
 endC="\033[0m\e[0m"
 #Colours
 clear
@@ -19,20 +19,20 @@ backSfwDet=$(screen -ls | grep 'Detached\|Attached' | awk $'{print $1,$4 }')
 linuxuptime=$(uptime | awk $'{print $3,$4,$5}')
 diskfreesp=$(df -H | grep sda2 | awk $'{print $4}')
 
-echo -e "\n${c02}[!]${endC}${c07} PC Status${endC}"
-echo -e "\n${c05}[+]${endC}${c07} User name:\t\t\t${endC}${c03}$(whoami)${endC}"
+echo -e "\n${co2}[!]${endC}${co7} PC Status${endC}"
+echo -e "\n${co5}[+]${endC}${co7} User name:\t\t\t${endC}${co3}$(whoami)${endC}"
 sleep 1
-echo -e "${c05}[+]${endC}${c07} Processor:      \t\t${endC}${c03}$cpuInfo ${endC}"
+echo -e "${co5}[+]${endC}${co7} Processor:      \t\t${endC}${co3}$cpuInfo ${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} Video:          \t\t${endC}${c03}$videoInfo ${endC}"
+echo -e "${co5}[+]${endC}${co7} Video:          \t\t${endC}${co3}$videoInfo ${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} Temperature:    \t\t${endC}${c03}$temp${endC}"
+echo -e "${co5}[+]${endC}${co7} Temperature:    \t\t${endC}${co3}$temp${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} Software:       \t\t${endC}${c06}$sfwInfo ${endC}"
+echo -e "${co5}[+]${endC}${co7} Software:       \t\t${endC}${co6}$sfwInfo ${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} IP Port:        \t\t${endC}${c03}$ipPort${endC}"
+echo -e "${co5}[+]${endC}${co7} IP Port:        \t\t${endC}${co3}$ipPort${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} Uptime:         \t\t${endC}${c03}$linuxuptime${endC}"
+echo -e "${co5}[+]${endC}${co7} Uptime:         \t\t${endC}${co3}$linuxuptime${endC}"
 sleep 0.2
-echo -e "${c05}[+]${endC}${c07} Free disk space:\t\t${endC}${c03}$diskfreesp${endC}"
+echo -e "${co5}[+]${endC}${co7} Free disk space:\t\t${endC}${co3}$diskfreesp${endC}"
 echo " "
