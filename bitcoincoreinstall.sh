3f425acc3a64dcd7020326e8e1fa58d2334c33d8
Lutxo <aleatoriosdenario@gmail.com>
2024-07-20 17:47:09 +0200

    Update bitcoincoreinstall.sh

--- bitcoincoreinstall.sh
+++ bitcoincoreinstall.sh
@@ -12,60 +12,60 @@ endco="\033[0m\e[0m"
    
 function_limpieza(){
    #pulizia iniziale
+   echo -e "${co5}[+] ${endco}${co8}Arresta Bitcoin core...${endco}"
    sudo bitcoin-cli stop > /dev/null
-   echo -e "${co5}[+] ${endco}${co8}Stopping Bitcoin core...${endco}"
    sleep 3
-   echo -e "${co5}[+] ${endco}${co8}Borrando Bitcoin core...${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Cancella Bitcoin core...${endco}"
    sudo rm -r -f /usr/local/bin/*
    sudo rm -r -f $HOME/.bitcoin/
 }
 
 function_herramientas(){
    #installare tools
-   echo -e "${co5}[+] ${endco}${co8}Instalando herramientas ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Install tools necessari ${endco}"
    sudo apt-get install -y bc jq autoconf file gcc libc-dev make g++ pkgconf re2c git libtool automake gcc xxd figlet
    sleep 1
 }
 
 function_dowload_btc(){
    #scarica bitcoin files
-   echo -e "${co5}[+] ${endco}${co8}Descargando Bitcoin core 24.1${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Scarica Bitcoin core 24.1${endco}"
    wget https://bitcoincore.org/bin/bitcoin-core-24.1/bitcoin-24.1-x86_64-linux-gnu.tar.gz > /dev/null
-   echo -e "${co5}[+] ${endco}${co8}Descargando checksum criptografico SHA256SUMS ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Scarica checksum crittografico SHA256SUMS ${endco}"
    wget https://bitcoincore.org/bin/bitcoin-core-24.1/SHA256SUMS > /dev/null
-   echo -e "${co5}[+] ${endco}${co8}Descargando firmas checksum ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Scarica firme checksum ${endco}"
    wget https://bitcoincore.org/bin/bitcoin-core-24.1/SHA256SUMS.asc > /dev/null 
-   echo -e "${co5}[+] ${endco}${co8}Descargando la lista de firmas ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Scarica l'elenco di firme ${endco}"
    git clone https://github.com/bitcoin-core/guix.sigs > /dev/null 
    sleep 1
 }
 
 function_verificar_firmas(){
    #verificare firme
-   echo -e "${co5}[+] ${endco}${co8}Verificando lista de checksum ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Verifica lista di checksum ${endco}"
    shasum --ignore-missing --check SHA256SUMS
-   echo -e "${co5}[+] ${endco}${co8}Importando todas las firmas ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Importa tutte le firme ${endco}"
    gpg --import guix.sigs/builder-keys/*
-   echo -e "${co5}[+] ${endco}${co8}Verificando el file checksum con varias firmas ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Verifica il file checksum con varie firmas ${endco}"
    gpg --verify SHA256SUMS.asc
-   echo -e "${co5}[!] ${endco}${co1}Verificar con los respectivos desarrolladores la validez de las firmas ! ${endco}"
+   echo -e "${co5}[!] ${endco}${co1}Verifica la validità delle fime con i corrispondenti svilupatori ! ${endco}"
 sleep 4
 }
 
 function_unzip_clean(){
    #scompattare pachetto bitcoin core
-   echo -e "\n${co5}[+] ${endco}${co8}Descomprimiendo Bitcoin Core 24.1${endco}"
-   tar xzf bitcoin-24.1-x86_64-linux-gnu.tar.gz
+   echo -e "\n${co5}[+] ${endco}${co8}Decomprime Bitcoin Core 24.1${endco}"
+   tar xzf bitcoin-24.1-x86_64-linux-gnu.tar.gz -v
    sleep 1
    #pulire files
-   echo -e "${co5}[+] ${endco}${co8}Eliminando files temporaneos ${endco}"
+   echo -e "${co5}[+] ${endco}${co8}Elimina files temporanei ${endco}"
    rm -r -f SHA256SUMS  SHA256SUMS.asc  bitcoin-24.1-x86_64-linux-gnu.tar.gz  guix.sigs
    sleep 1 
 }
 
 function_instala(){
   #instalare binari
-  echo -e "${co5}[+] ${endco}${co2}Instalando Bitcoin Core 24.1 ${endco}"
+  echo -e "${co5}[+] ${endco}${co2}Installa Bitcoin Core 24.1 ${endco}"
   sudo install -m 0755 -o root -g root -t /usr/local/bin/ bitcoin-24.1/bin/*
   rm -r -f bitcoin*
   sleep 2
@@ -78,6 +78,8 @@ function_conf(){
   echo $(cat $HOME/.bitcoin/bitcoin.conf)
   sleep 1
   echo -e "${co5}[!] ${endco}${co1}$(bitcoind --version | grep version ) ${endco}"
+  echo -e "${co5}[+] ${endco}${co2}Contenuto del file bitcoin.conf ${endco}"
+  cat $HOME/.bitcoin/bitcoin.conf 
 }
 
 function_limpieza
